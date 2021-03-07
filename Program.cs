81e7ceebd21103bc54d044444a6301f98e55da25
SILVANI CUSTODIO DE OLIVEIRA <silvani.oliveira@etec.sp.gov.br>
2021-03-07 13:54:10 -0300

    C# Pronto

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace pc_etec1b_projeto_caixa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("+---------+");
+            Console.WriteLine("| Silvani |");
+            Console.WriteLine("+---------+");
+            Console.ReadKey();
+        }
+    }
+}
